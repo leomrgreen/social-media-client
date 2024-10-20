c487ff52b506cfcd16d2b74f88f2b1e90740471e
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-10-20 16:04:32 +0200

    added test for logging in with invalid credentials for e2e testing

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -22,7 +22,7 @@ describe('Login & Logout test', () => {
     cy.wait('@login').its('response.statusCode').should('eq', 200);
   });
 
-  it('should store token in local storage after login', () => {
+  it('should store token in local storage after logging in with valid credentials', () => {
     // Check if token is stored in localStorage
     cy.window().then((win) => {
       expect(win.localStorage.getItem('token')).to.exist;
@@ -41,6 +41,33 @@ describe('Login & Logout test', () => {
     // Check if token is properly removed from LocalStorage
     cy.window().then((win) => {
       expect(win.localStorage.getItem('token')).to.be.null;
+    });
+  });
+});
+
+describe('Invalid login test', () => {
+  it('should fail the login (error 401 - unauthorized) and show an error message', () => {
+    // Visit the login page
+    cy.visit('/index.html');
+
+    // Fill out the login form using IDs
+    cy.get('#loginEmail').type('fail@stud.noroff.no', { force: true });
+    cy.get('#loginPassword').type('invalidPassword', { force: true });
+
+    // Intercept the login request
+    cy.intercept(method, API_URL).as('login');
+
+    // Click on the login button using ID
+    cy.get('#loginForm').submit();
+
+    // Wait for the login request to fail (err 401)
+    cy.wait('@login').its('response.statusCode').should('eq', 401);
+
+    // Making sure the user is alerted with an error message
+    cy.on('window:alert', (msg) => {
+      expect(msg).to.contains(
+        'Either your username was not found or your password is incorrect',
+      );
     });
   });
 });
